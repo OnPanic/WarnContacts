d8847b6849f494c4d883048654a0a173ac0bbf5c
arrase <arrase@gmail.com>
2016-12-27 02:24:30 +0100

    refactor

--- app/src/main/java/org/thepanicproject/warncontacts/constants/WarnConstants.java
+++ app/src/main/java/org/thepanicproject/warncontacts/constants/WarnConstants.java
@@ -2,8 +2,9 @@ package org.thepanicproject.warncontacts.constants;
 
 
 public class WarnConstants {
-    public static final int CONTACT_PICKER_RESULT = 0x001;
     public static final String CONTACT_URI = "CONTACT_URI";
+
+    public static final int CONTACT_PICKER_RESULT = 0x001;
     public static final int REQUEST_READ_CONTACTS = 0x002;
     public static final int REQUEST_LOCATION_PERMISSION = 0x003;
 }
