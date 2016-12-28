09f58421152b95b6e6bac8b13326cfa02ca9f33c
arrase <arrase@gmail.com>
2016-12-29 00:47:54 +0100

    basic trigger response

--- app/src/main/java/org/thepanicproject/warncontacts/activities/PanicResponseActivity.java
+++ app/src/main/java/org/thepanicproject/warncontacts/activities/PanicResponseActivity.java
@@ -1,19 +1,33 @@
 package org.thepanicproject.warncontacts.activities;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import org.thepanicproject.warncontacts.R;
+import org.thepanicproject.warncontacts.notifications.TriggerNotification;
 
 import info.guardianproject.panic.PanicResponder;
 
 public class PanicResponseActivity extends Activity {
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
         if (PanicResponder.receivedTriggerFromConnectedApp(this)) {
-            // TODO
-        } else {
-            ExitActivity.exitAndRemoveFromRecentApps(this);
+            if (prefs.getBoolean(getString(R.string.pref_runned_notification), true)) {
+                TriggerNotification notification = new TriggerNotification(getApplicationContext());
+                notification.show();
+            }
+
+            ExitActivity.exitAndRemoveFromRecentApps(PanicResponseActivity.this);
         }
+
+        finish();
     }
 }
